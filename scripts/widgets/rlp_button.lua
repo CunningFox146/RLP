6dfae314c0ed5f61cef2823895bdfed33aefa070
FBNikita <funnybunnynikita@mail.ru>
2017-12-19 19:55:23 +0300

    Revert "Revert "19.12.2017""
    This reverts commit 7339e10a56fa9ce041936ff0f5a651f1542b2d6f.

--- scripts/widgets/rlp_button.lua
+++ scripts/widgets/rlp_button.lua
@@ -8,28 +8,27 @@ local OFFSET = -5.5
 local RLPButton = Class(Widget, function(self)
     Widget._ctor(self, "RLPButton")
 	
-	--self:SetPosition(1040, 124 , 0)
+    self.btn = self:AddChild(ImageButton("images/global_redux.xml",
+       "button_carny_square_normal.tex",
+       "button_carny_square_hover.tex",
+       "button_carny_square_disabled.tex",
+       "button_carny_square_down.tex"))
 	
-	self.btn = self:AddChild(ImageButton("images/frontend.xml", "button_square.tex", "button_square_halfshadow.tex", "button_square_disabled.tex", "button_square_halfshadow.tex", "button_square_disabled.tex", {1,1}, {0,0}))
-	self.btn.image:SetScale(.7)
+	self.btn:SetFocusScale(1.05, 1.05, 1.05)
+	self.btn:SetNormalScale(.7, .7, 1)
 
 	self.btn.icon = self.btn:AddChild(Image("images/button_icons.xml", "mods.tex", "mods.tex"))
 	self.btn.icon:SetPosition(-5,4)
 	self.btn.icon:SetScale(.16)
 	self.btn.icon:SetClickable(false)
 
-	self.btn.highlight = self.btn:AddChild(Image("images/frontend.xml", "button_square_highlight.tex"))
-	self.btn.highlight:SetScale(.7)
-	self.btn.highlight:SetClickable(false)
-	self.btn.highlight:Hide()
-
 	self.btn:SetTextSize(25)
 	self.btn:SetText("Настроить\nрусификатор.", true)
 	self.btn.text:SetPosition(-3, -34 + OFFSET)
 	self.btn.text_shadow:SetPosition(-5, -36 + OFFSET)
 	self.btn:SetFont(NEWFONT_OUTLINE)
-	self.btn:SetTextColour({unpack(GOLD)})
-	self.btn:SetTextFocusColour({1,1,1,1})
+	self.btn:SetTextColour(UICOLOURS.GOLD)
+	self.btn:SetTextFocusColour(UICOLOURS.GOLD)
 	
 	self.btn:SetOnClick(function()
 		TheFrontEnd:GetSound():KillSound("FEMusic")
@@ -37,16 +36,6 @@ local RLPButton = Class(Widget, function(self)
 		TheFrontEnd:GetSound():PlaySound("dontstarve/music/gramaphone_ragtime", "rlp_ragtime") 
 		
 		TheFrontEnd:FadeToScreen(TheFrontEnd:GetActiveScreen(), function() return LanguageOptions() end)
-	end)
-
-	self.btn:SetOnGainFocus(function()
-		if self.btn:IsEnabled() and not self.btn:IsSelected() and TheFrontEnd:GetFadeLevel() <= 0 then
-			self.btn.highlight:Show()
-		end
-	end)
-	
-	self.btn:SetOnLoseFocus(function()
-		self.btn.highlight:Hide()
 	end)
 end)
 
