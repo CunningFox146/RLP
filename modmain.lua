6c7813477f1e9691b5deeccb85b8dfc090b9fa0f
Graph <seserega@gmail.com>
2017-12-22 04:22:07 +0300

    Вылетало при загрузке мира

--- modmain.lua
+++ modmain.lua
@@ -2981,11 +2981,11 @@ if t.CurrentTranslationType~=t.TranslationTypes.ChatOnly then --Выполняе
 		local oldupdate_fn=self.update_fn
 		self.update_fn=function(context, widget, data, index)
 			oldupdate_fn(context, widget, data, index)
-			if data and data.days_survived then
+			if data and data.days_survived and widget.DAYS_LIVED then
 				local Text = require "widgets/text"
 				widget.DAYS_LIVED:SetTruncatedString((data.days_survived or STRINGS.UI.MORGUESCREEN.UNKNOWN_DAYS).." "..StringTime(data.days_survived), widget.DAYS_LIVED._align.maxwidth, widget.DAYS_LIVED._align.maxchars, true)
 			end
-			if data and data.playerage then
+			if data and data.playerage and widget.PLAYER_AGE then
 				local Text = require "widgets/text"
 				local age_str = (data.playerage or STRINGS.UI.MORGUESCREEN.UNKNOWN_DAYS).." "..StringTime(tonumber(data.playerage))
 				widget.PLAYER_AGE:SetTruncatedString(age_str, widget.PLAYER_AGE._align.maxwidth, widget.PLAYER_AGE._align.maxchars, true)
