7d3b874f9a84be8f22451e9789dcd8d6a4eff4d2
CunningFox <makar.papca@gmail.com>
2018-01-12 19:39:16 +0300

    1.2.1

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name = "Russification Pack for DST"
-version = "1.2.0"
+version = "1.2.1"
 
 local desc = {
 	en = "\nThe best russian localisation at the moment. Soure code was taken from the Russian Language Pack. Special thanks to Some1 and Alevastor!\n\nVersion: "..version,
